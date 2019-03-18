19ee5558b3c91fca981d4363596079e2ed89d491
juloko <julio_flima@hotmail.com>
2019-03-18 10:31:01 -0300

    design of betters

--- functions/getter/getReadable.js
+++ functions/getter/getReadable.js
@@ -1,37 +1,29 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const request = require('request');
-const cheerio = require('cheerio');
+const axios = require('axios');
+const https = require('https')
 
 exports = module.exports = functions.https.onRequest((req, res) => {
     res.set("Access-Control-Allow-Origin", "*");
-    var result;
-    if(req.query.funcRead === undefined){
+    if (req.query.funcRead === undefined) {
         result = req.body;
-    }else{
+    } else {
         result = req.query;
     }
-    let funcRead = result.funcRead;
-    const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9';
-    let options = {
-        url: 'http://187.19.164.236:9001/readable',
-        method: 'POST',
-        headers: {
-            'User-Agent': userAgent,
-            'accept': 'application/json'
-        },
-        data: {
-            'funcRead': "listEvents"
-        }
-    };
-    request(options, (error, response, html) => {
-        if (!error) {
-            page = cheerio.load(html);
-            dataReceived = JSON.parse(page.text());
-            res.send(dataReceived);
-        } else {
-            console.log(error,response,html);
-            res.send(error,response,html);
-        }
-    });
+    var require = result.funcRead
+    var filtered = JSON.stringify(result.filter)
+    var lang = result.locale
+    var hereURL = `https://127.0.0.1:9001/readable?funcRead=${require}&filter=${filtered}&locale=${lang}`;
+    var thereURL = `https://187.19.164.236:9001/readable?funcRead=${require}&filter=${filtered}&locale=${lang}`;
+    axios.post(thereURL,
+        {
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false
+            }),
+        }).then(response => {
+            throw res.send(response.data);
+        }).catch(error => {
+            throw console.log(error);
+        });
 });
--- public/js/betfair.js
+++ public/js/betfair.js
@@ -1,23 +1,87 @@
-function games() {
-    $.ajax({
-        url: 'https://us-central1-brother-bet.cloudfunctions.net/getReadable',
-        dataType: "json",
-        method: 'POST',
-        crossDomain: true,
-        headers: {
-            'Accept': 'application/json'
-        },
-        data: {
-            funcRead: 'listEvents'
-        },
-        success: function (data) {
-            $('#games').html(JSON.stringify(data))
-        }
+function getReadable(dataIn) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: 'https://us-central1-brother-bet.cloudfunctions.net/getReadable',
+            dataType: "json",
+            method: 'POST',
+            crossDomain: true,
+            headers: {
+                'Accept': 'application/json'
+            },
+            data: {
+                'funcRead': dataIn.funcRead,
+                'filter': JSON.stringify(dataIn.filter),
+                'locale': dataIn.locale
+            }
+        }).done((response) => {
+            resolve(response);
+        }).fail((error) => {
+            reject(error);
+        });
+    });
+}
+function receiveGames() {
+    getReadable({
+        funcRead: "listEvents",
+        filter: { "eventTypeIds": ["1"] },
+        locale: "en"
+    }).then(data => {
+        data[2].result.forEach(element => {
+            $('#games').find('table tr:last').after(
+                `<tr>
+                    <td>
+                        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-data-table__select mdl-js-ripple-effect--ignore-events is-upgraded"
+                        data-upgraded=",MaterialCheckbox,MaterialRipple">
+                            <input type="checkbox" class="mdl-checkbox__input">
+                            <span class="mdl-checkbox__focus-helper"></span><span class="mdl-checkbox__box-outline">
+                            <span class="mdl-checkbox__tick-outline"></span></span>
+                            <span class="mdl-checkbox__ripple-container mdl-js-ripple-effect mdl-ripple--center" data-upgraded=",MaterialRipple">
+                            <span class="mdl-ripple"></span></span>
+                        </label>
+                    </td>
+                    <td id="${element.event.id}" class="mdl-data-table__cell--non-numeric">${element.event.name}</td>                    
+                    <td>${element.event.countryCode || ""}</td>   
+                    <td>${element.event.openDate || ""}</td>
+                    <td>${element.event.timezone || ""}</td>
+                    <td>${element.event.venue || ""}</td>
+                    <td>${element.marketCount || ""}</td>
+                </tr>`
+            );
+            $('#tchanTiped').hide();
+            $('#games').show();
+        });
     });
 }
 
 // Main.
-document.addEventListener('DOMContentLoaded', async function () {
-    games()
-});
+(() => {
+    //Get betters.
+    getReadable({
+        funcRead: "listEventTypes",
+        filter: {},
+        locale: "en"
+    }).then(data => {
+        data[2].result.forEach(element => {
+            $('#betters').append(
+                `<span id="${element.eventType.id}" class="mdl-chip mdl-chip--contact">
+                    <span class="mdl-chip__contact  mdl-color--purple-500 mdl-color-text--white">${element.eventType.name.substring(0, 2)}</span>
+                    <span class="mdl-chip__text">${element.eventType.name.split(" ")[0]}</span>
+                </span>
+                <div id="${element.eventType.id}-tooltip"class="mdl-tooltip" for="${element.eventType.id}">
+                ${element.eventType.name}
+                </div>`
+            )
+            componentHandler.upgradeElement(document.getElementById(`${element.eventType.id}-tooltip`));
+            componentHandler.upgradeElement(document.getElementById(`${element.eventType.id}`));
+            $(" ").click(function () {
+                console.log(this.id);
+                $('#' + this.id + ' .mdl-chip--contact').css("background-color", "green")
+            });
+        });
+    });
+}).call()
+
+
+
+
 
