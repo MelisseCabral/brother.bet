77dde91b9ec4bb3dee34564e60b8431cdeb1fd70
Julio Lima <julio_flima@hotmail.com  git config --global user.name Your>
2020-07-28 19:31:12 -0300

    Remove dist with map.

--- functions/views/webpack.config.js
+++ functions/views/webpack.config.js
@@ -5,89 +5,93 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  target: 'web',
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-  },
-  entry: './src/js/main',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
+module.exports = (env) => {
+  const mode = env.production ? 'production' : 'development';
+  const sourceMap = env.production ? false : 'source-map';
+  return {
+    mode,
+    devtool: sourceMap,
+    target: 'web',
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    entry: './src/js/init',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
               // only enable hot in development
-              hmr: process.env.NODE_ENV === 'development',
-              // if hmr does not work, this is a forceful method.
-              reloadAll: true,
+                hmr: process.env.NODE_ENV === 'development',
+                // if hmr does not work, this is a forceful method.
+                reloadAll: true,
+              },
             },
-          },
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+            'css-loader',
+          ],
         },
-      },
-      {
-        test: /\.(jpg?g|png|gif|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'images/',
-        },
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              // name: '[name].[ext]',
-              outputPath: 'fonts/',
+              presets: ['@babel/preset-env'],
             },
           },
-        ],
-      },
+        },
+        {
+          test: /\.(jpg?g|png|gif|svg)$/i,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+          },
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+              // name: '[name].[ext]',
+                outputPath: 'fonts/',
+              },
+            },
+          ],
+        },
 
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html',
+      }),
+      new HtmlWebpackPlugin({
+        filename: './components/statistics.html',
+        template: './src/components/statistics.html',
+      }),
+      new HtmlWebpackPlugin({
+        filename: './components/tableRanking.html',
+        template: './src/components/tableRanking.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery',
+        'window.jQuery': 'jquery',
+        'window.$': 'jquery',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      filename: './components/statistics.html',
-      template: './src/components/statistics.html',
-    }),
-    new HtmlWebpackPlugin({
-      filename: './components/tableRanking.html',
-      template: './src/components/tableRanking.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      $: 'jquery',
-      'window.jQuery': 'jquery',
-      'window.$': 'jquery',
-    }),
-  ],
+  };
 };
