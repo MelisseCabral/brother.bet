c505d13e86ce3b6924793c43ce28ace1274f1ae9
juloko <julio_flima@hotmail.com  git config --global user.name Your>
2020-08-20 08:33:09 -0300

    Add script to install in local server.

---
+++ functions/model/server/script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# install the latest version of Chrome and the Chrome Driver
+#Make the script executable with command chmod +x <fileName>.
+apt-get update && apt-get install -y libnss3-dev
+version=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N http://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin
+chmod +x /usr/local/bin/chromedriver
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
