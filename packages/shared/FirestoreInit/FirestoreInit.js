056e306eb814c6ec15e1bae3a0cd0e568fe61601
juloko <julio_flima@hotmail.com  git config --global user.name Your>
2020-08-21 20:10:37 -0300

    Ignore files.

---
+++ packages/shared/FirestoreInit/FirestoreInit.js
@@ -0,0 +1,26 @@
+const admin = require('firebase-admin');
+
+class FirestoreInit {
+  constructor(serviceAccount) {
+    this.admin = admin;
+    this.serviceAccount = serviceAccount;
+    this.projectId = this.serviceAccount.project_id;
+    this.databaseURL = `https://${this.projectId}.firebaseio.com`;
+
+    this.initialize();
+    this.configFirestore();
+  }
+
+  initialize() {
+    this.admin.initializeApp({
+      credential: admin.credential.cert(this.serviceAccount),
+      databaseURL: this.databaseURL,
+    });
+  }
+
+  configFirestore() {
+    this.admin.firestore().settings({ timestampsInSnapshots: true });
+  }
+}
+
+module.exports = new FirestoreInit();
